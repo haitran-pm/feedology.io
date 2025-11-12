e8f39bf5594b68de613aca1d1562a4f8a1c85ac0
Hai Tran <haitran@192.168.1.7>
2025-11-12 17:38:48 +0700

    Remove unused files

--- src/components/sections/header.tsx
+++ src/components/sections/header.tsx
@@ -10,7 +10,6 @@ import {
 	Stack,
 	Typography,
 	Grid,
-	useColorScheme,
 } from '@mui/material';
 
 import { IconMenu, IconX as IconClose } from '@tabler/icons-react';
@@ -174,7 +173,6 @@ interface NavigationLink {
 }
 
 function LogoWrapper() {
-	const { mode } = useColorScheme();
 	return (
 		<Link href="/" underline="none">
 			<Stack alignItems="center" direction="row" spacing={1.5}>
